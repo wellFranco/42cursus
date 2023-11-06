51ff33483f13156c5274bc26c6f6a8f5ca551ad0
wfranco <huntergamma250@gmail.com>
2023-11-06 17:57:13 -0300

    printf começo

---
+++ 42cursus/printf/libftprintf.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wfranco <wfranco@student.42.rio>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/06 16:02:17 by wfranco           #+#    #+#             */
+/*   Updated: 2023/11/06 16:17:52 by wfranco          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+# include <unistd.h>
+
+int	ft_putchar(char c);
+
+int	ft_putstr(char *s);
+
+int	ft_putnbr(int n);
+
+#endif
