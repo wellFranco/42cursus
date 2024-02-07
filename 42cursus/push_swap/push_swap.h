abf0cd69ae24d2a1e5c19ea7697f75a655508925
wfranco <huntergamma250@gmail.com>
2024-02-07 20:52:36 -0300

    primeiros passos

---
+++ 42cursus/push_swap/push_swap.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wfranco <wfranco@student.42.rio>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/07 16:03:31 by wfranco           #+#    #+#             */
+/*   Updated: 2024/02/07 16:06:38 by wfranco          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <stdbool.h>
+# include <stdlib.h>
+
+typedef struct s_stack
+{
+	long			nbr;
+	long			index;
+	struct s_stack	*next;
+	struct s_stack *prev;
+}	t_stack;
+
+#endif
